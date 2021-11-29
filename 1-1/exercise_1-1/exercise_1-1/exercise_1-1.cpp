929249270ddafc4c6025528fa1b1c2a4c0ab76d9
BlackOwl <Be_lusky@mail.ru>
2021-11-29 15:39:57 +0300

    Решена задача 1-1

---
+++ 1-1/exercise_1-1/exercise_1-1/exercise_1-1.cpp
@@ -0,0 +1,22 @@
+﻿#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const double x = 0.2;
+    const double y = 0.004;
+    const double z = 1.1;
+    double a = pow((x * x + z), 2); //pow-функция возводящая в квадрат
+    double b = pow(sin(a), 3);
+    double c = b - sqrt(x / y);
+    double d = ((x * x) / z) + cos(x * x * x + y * y * y);
+    cout << c << endl;
+    cout << d;
+    return 0;
+
+}
+
+
