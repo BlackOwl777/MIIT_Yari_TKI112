0ba59f2192f2adba85cd165404a2bf5fcd03fe8e
BlackOwl <Be_lusky@mail.ru>
2021-12-01 01:14:57 +0300

    Создано решение задания 1-3

---
+++ 1-3/exercise_1-3/exercise_1-3/exercise_1-3.cpp
@@ -0,0 +1,22 @@
+﻿#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "rus"); //перевод на русские буквы
+	cout<< "Введите силу тяги - F: ";
+	double f = 0;
+	cin >> f;
+	cout << "Ведите значиние работы - A: ";
+	double a = 0;
+	cin >> a;
+
+	double way = (a * 1000000) / (f * 1000);
+
+	cout << "Путь пройденный автомобилем равен: " << way << endl;
+	return 0;
+}
