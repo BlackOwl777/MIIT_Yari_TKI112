3da877df4a8b09db4cfb88ae23c539af971ad38d
BlackOwl <Be_lusky@mail.ru>
2021-12-01 01:06:00 +0300

    Создано решение 1-2

---
+++ 1-2/exercise_1-2/exercise_1-2/exercise_1-2.cpp
@@ -0,0 +1,28 @@
+﻿#define _USE_MATH_DEFINES
+#include <cmath>
+#include <math.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "rus");
+	double H;
+	cout << "Введите высоту конуса: ";
+	cin >> H;
+	double L;
+	cout << "Длину образующей конуса: ";
+	cin >> L;
+	double R = H / 2;
+	double S = (M_PI * (R * R)) + (M_PI * R * L);
+	cout << "Площадь полной поверхности конуса: ";
+	cout << S << endl;
+	double V = 0.3333333333 * H * M_PI * (R * R);
+	cout << "Объём: ";
+	cout << V << endl;
+	return 0;
+}
+
+
