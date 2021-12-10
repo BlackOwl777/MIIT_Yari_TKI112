01b6470247670514a107ca12fc21ee6740161e09
BlackOwl777 <44806268+BlackOwl777@users.noreply.github.com>
2021-12-10 19:51:15 +0300

    Update 3-1.cpp

--- 3-1/exercise_3-1/exercise_3-1/3-1.cpp
+++ 3-1/exercise_3-1/exercise_3-1/3-1.cpp
@@ -1,17 +1,26 @@
-﻿#include<iostream>
+#include <iostream>
 #include <iomanip>
-#include<cmath>
-
+#include <cmath>
 using namespace std;
 
-double f(double x) {
-	double ans = tan(x) - ((1 / 3) * pow(tan(x), 3)) + (0.2 * pow(tan(x), 5)) - (1 / 3);
-	return (ans);
+/*
+* \brief вычисление значения функции
+* \param x значение переменной x
+* \return значение функции
+*/
+double getY(const double x);
+
+
+int main() 
+{
+  cout << fixed << setprecision(5);
+  for (double x = -10; x <= 0.81; x += 0.05) 
+    cout << "x = " << x << " " << "y = " << getY(x) << " " << "\n";
+  return (0);
 }
 
-int main() {
-	cout << fixed;
-	for (double x = 0; x <= 0.81; x += 0.05) {
-		cout << setprecision(5) << "y = " << f(x) << " " << "x = " << x << endl;
-	}
+double getY(const double x) 
+{
+  double ans = tan(x) - ((1 / 3) * pow(tan(x), 3)) + (0.2 * pow(tan(x), 5)) - (1 / 3);
+  return (ans);
 }
