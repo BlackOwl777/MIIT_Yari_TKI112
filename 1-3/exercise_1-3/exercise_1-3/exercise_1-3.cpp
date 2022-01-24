a9ae825ae3f9cd7ea55ac525552a1eddf4c4751a
BlackOwl777 <44806268+BlackOwl777@users.noreply.github.com>
2022-01-24 22:34:06 +0300

    Update exercise_1-3.cpp

--- 1-3/exercise_1-3/exercise_1-3/exercise_1-3.cpp
+++ 1-3/exercise_1-3/exercise_1-3/exercise_1-3.cpp
@@ -11,7 +11,10 @@ using namespace std;
 * @return путь пройденный автомобилем
 */
 double getWay(const double work, const double force);
-
+/*
+* @brief точка входа в программу
+* @return 0 в случае успеха
+*/
 
 
 int main()
@@ -30,5 +33,5 @@ int main()
 
 double getWay(const double work, const double force)
 {
-	return ((work * 1000000) / (force * 1000));
+	return ((work * 1000000) / (force * 1000)); //work ѕеревод работы из мегаджоулей в джоули,force ѕеревод силы из килоньютонов в ньютоны
 }
