f864a0cd88364ee4116f9c20312ff80ef64ab852
BlackOwl777 <44806268+BlackOwl777@users.noreply.github.com>
2022-02-17 16:13:00 +0300

    Update 4-1.cpp

--- 4-1/4-1/4-1.cpp
+++ 4-1/4-1/4-1.cpp
@@ -6,9 +6,10 @@
 using namespace std;
 
 /**
-*\brief Нахождение минимального по значению элемента
+*\brief Нахождение индекс минимального элемента
 *\param array Массив
 *\param size Размер массива
+*\return Минимальное значение
 **/
 size_t GetMin(const int* array, const size_t size);
 
@@ -16,15 +17,16 @@ size_t GetMin(const int* array, const size_t size);
 *\brief Нахождение среднего арифметического среди элементов массива
 *\param array Массив
 *\param size Размер массива
+*\return Среднее арифметическое
 **/
-int GetAverage(const int* array, const size_t size);
+double GetAverage(const int* array, const size_t size);
 
 /**
 *\brief Замена минимального элемента массива на среднее арифметическое
 *\param array Массив
 *\param size Размер массива
 **/
-void MinToAverage(int* array, const size_t size);
+void  MinToAverage(int* array, const size_t size);
 
 /**
 *\brief Вывод индексов элементов, значение которых больше предыдущих
@@ -36,6 +38,7 @@ void ShowIndex(int* array, const size_t size);
 *\brief Проверка на существование двух пар элементов с одинаковыми знаками
 *\param array Массив
 *\param size Размер массива
+*\return Проверенный элемент
 **/
 bool CheckElement(int* array, const size_t size);
 
@@ -44,8 +47,8 @@ bool CheckElement(int* array, const size_t size);
 **/
 enum class Input
 {
-	randomize = 1,
-	user = 2
+    RANDOM = 1,
+    MANUAL = 2
 };
 
 /**
@@ -55,155 +58,156 @@ enum class Input
 * \param min Минимально возможное число
 * \param max Максимально возможное число
 **/
-void randomizeArray(int* array, const size_t size, const int min, const int max);
+void RandomizeArray(int* array, const size_t size, const int min, const int max);
 
 /**
 * \brief Ввод массива с клавиатуры
 * \param array Массив
 * \param size Размер массива
 **/
-void userInput(int* array, const size_t size);
+void UserInput(int* array, const size_t size);
 
 /**
 * \brief Конвертация массива строку вида {element_1, element_2, ... ,element_n}
 * \param array Массив
 * \param size Размер массива
 * \param message Сообщение, которое будет выведено перед массивом
+* \return Значение функции
 **/
-string arrayToString(int* array, const size_t size, const string& message);
+string ArrayToString(int* array, const size_t size, const string& message);
 
 int main() {
 
-	SetConsoleOutputCP(1251);
-	SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
 
-	const int min = -20, max = 40;
+    const int min = -20, max = 40;
 
-	cout << "Введите размер массива\n";
-	size_t size,choice;
-	cin >> size;
-	int* array = new int[size];
+    cout << "Введите размер массива\n";
+    size_t size;
+    cin >> size;
+    size_t size_array = size;
+    int* array = new int[size_array];
 
-	cout << "Выберите способ заполнения массива:";
-	cout << "Случайные элементы - " << static_cast<int>(Input::randomize);
-	cout << "Ручной ввод - " << static_cast<int>(Input::user);
-	cin >> choice;
+    cout << "Выберите способ заполнения массива:\n" << "Случайные элементы - 1\n" << "Ручной ввод - 2\n";
 
-	const auto input = static_cast<Input>(choice);
+    int choice;
+    cin >> choice;
 
-	switch (input)
-	{
-	case Input::randomize:
-	{
-		randomizeArray(array, size, min, max);
-		cout << arrayToString(array, size, "массив = ") << "\n";
-		break;
-	}
-	case Input::user:
-	{
-		userInput(array, size);
-		cout << arrayToString(array, size, "массив = ") << "\n";
-		break;
-	}
-	default:
-		break;
-	}
+    const auto input = static_cast<Input>(choice);
 
-	cout << "Индексы элементов, значения которых больше предыдущих\n";
+    switch (input)
+    {
+    case Input::RANDOM:
+    {
+        RandomizeArray(array, size_array, min, max);
+        cout << ArrayToString(array, size, "массив = ") << "\n";
+        break;
+    }
+    case Input::MANUAL:
+    {
+        UserInput(array, size_array);
+        cout << ArrayToString(array, size, "массив = ") << "\n";
+        break;
+    }
+    default:
+        break;
+    }
 
-	ShowIndex(array, size);
+    cout << "Индексы элементов, значения которых больше предыдущих\n";
 
-	if (CheckElement(array, size))
-		cout << "существует две пары соседних элементов с одинаковыми знаками\n";
-	else
-		cout << "не существует две пары соседних элементов с одинаковыми знаками\n";
+    ShowIndex(array, size_array);
 
-	MinToAverage(array, size);
-	cout << arrayToString(array, size, "Измененный массив = ") << '\n';
+    if (CheckElement(array, size_array))
+        cout << "существует две пары соседних элементов с одинаковыми знаками\n";
+    else
+        cout << "не существует две пары соседних элементов с одинаковыми знаками\n";
 
-	if (array != nullptr)
-	{
-		delete[] array;
-		array = nullptr;
-	}
+    MinToAverage(array, size_array);
+    cout << ArrayToString(array, size_array, "Измененный массив = ") << '\n';
+
+    if (array != nullptr)
+    {
+        delete[] array;
+        array = nullptr;
+    }
 }
 
-void randomizeArray(int* array, const size_t size, const int min, const int max)
+void RandomizeArray(int* array, const size_t size, const int min, const int max)
 {
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	const uniform_int_distribution<> uniformIntDistribution(min, max);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    const uniform_int_distribution<> uniformIntDistribution(min, max);
 
-	for (size_t i = 0; i < size; ++i)
-		array[i] = uniformIntDistribution(gen);
+    for (size_t i = 0; i < size; ++i)
+        array[i] = uniformIntDistribution(gen);
 
 }
 
-void userInput(int* array, const size_t size)
+void UserInput(int* array, const size_t size)
 {
-	for (size_t i = 0; i < size; i++)
-	{
-		cout << "array[" << i << "] = ";
-		cin >> array[i];
-	}
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << "array[" << i << "] = ";
+        cin >> array[i];
+    }
 }
 
-string arrayToString(int* array, const size_t size, const string& message = "")
+string ArrayToString(int* array, const size_t size, const string& message = "")
 {
-	stringstream str;
-	str << message << "{" << array[0];
+    stringstream str;
+    str << message << "{" << array[0];
 
-	for (size_t i = 1; i < size; ++i)
-	{
-		str << ", " << array[i];
-	}
+    for (size_t i = 1; i < size; ++i)
+    {
+        str << ", " << array[i];
+    }
 
-	str << "}";
+    str << "}";
 
-	return
-		str.str();
+    return str.str();
 }
 
 size_t GetMin(int* array, size_t size) {
-	size_t minIndex = 0;
+    size_t minIndex = 0;
 
-	for (size_t i = 1; i < size; i++)
-	{
-		if (array[minIndex] > array[i])
-			minIndex = i;
-	}
+    for (size_t i = 1; i < size; i++)
+    {
+        if (array[minIndex] > array[i])
+            minIndex = i;
+    }
 
-	return minIndex;
+    return minIndex;
 }
 
-int GetAverage(int* array, size_t size) {
-	int average = 0;
-	for (size_t i = 1; i < size; i++) {
-		average += array[i];
-	}
-	return average / int (size);
+double GetAverage(int* array, size_t size) {
+    double average = 0;
+    for (size_t i = 1; i < size; i++) {
+        average += array[i];
+    }
+    return average / size;
 }
 
-void MinToAverage(int* array, size_t size)
+void  MinToAverage(int* array, size_t size)
 {
-	array[GetMin(array, size)] = GetAverage(array, size);
+    array[GetMin(array, size)] = GetAverage(array, size);
 }
 
 void ShowIndex(int* array, size_t size) {
-	if (size <= 2)
-		return;
+    if (size <= 2)
+        return;
 
-	for (size_t i = 1; i < size - 1; i++)
-	if (array[i] < array[i + 1])
-		cout << "index = " << i + 1 << '\n';
+    for (size_t i = 1; i < size - 1; i++)
+        if (array[i] < array[i + 1])
+            cout << "index = " << i + 1 << '\n';
 
 }
 
 bool CheckElement(int* array, size_t size) {
-	int k = 0;
+    int k = 0;
 
-	for (size_t i = 0; i < size; i++)
-	if (array[i] * array[i + 1] > 0)
-		k++;
-	return k >= 2;
+    for (size_t i = 0; i < size; i++)
+        if (array[i] * array[i + 1] > 0)
+            k++;
+    return k >= 2;
 }
